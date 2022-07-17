1819c546990dceb7c5880fc6974dff74f6889d45
black996 <rharasis@yahoo.com>
2022-07-17 23:50:18 +0300

    implement external mock module with jest

---
+++ mocking/__mocks__/hikingPredictor.js
@@ -0,0 +1,3 @@
+module.exports = {
+    isGoodDayToHike: jest.fn(() => true)
+}
---
+++ mocking/__tests__/external_mock_module.test.js
@@ -0,0 +1,15 @@
+const willIHikeToday = require("../willIHikeToday");
+const hikingPredictor = require("../hikingPredictor");
+
+
+jest.mock('../hikingPredictor')
+test('returns if you can hike today', () => {
+    const result = willIHikeToday();
+
+    expect(result).toBe("Yes!");
+    expect(hikingPredictor.isGoodDayToHike).toHaveBeenCalled();
+    expect(hikingPredictor.isGoodDayToHike).toHaveBeenCalledTimes(1);
+
+    //cleanup
+    hikingPredictor.isGoodDayToHike.mockReset();
+})
