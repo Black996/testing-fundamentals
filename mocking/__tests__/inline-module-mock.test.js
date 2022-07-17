35aaad3c7c1f7cfeac93b6433c0bae9767855400
black996 <rharasis@yahoo.com>
2022-07-17 23:27:06 +0300

    implement inline module mocking using jest

---
+++ mocking/__tests__/inline-module-mock.test.js
@@ -0,0 +1,16 @@
+// we can achieve mocking the function with spy utility from jest as well
+const willIHikeToday = require("../willIHikeToday");
+const hikingPredictor = require("../hikingPredictor");
+
+jest.mock('../hikingPredictor', () => ({ isGoodDayToHike: jest.fn(() => true) }))
+
+test('returns if you can hike today', () => {
+    const result = willIHikeToday();
+
+    expect(result).toBe("Yes!");
+    expect(hikingPredictor.isGoodDayToHike).toHaveBeenCalled();
+    expect(hikingPredictor.isGoodDayToHike).toHaveBeenCalledTimes(1);
+
+    //cleanup
+    hikingPredictor.isGoodDayToHike.mockReset();
+})
